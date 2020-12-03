df951bae11c5de31d6156453e9ede63a5d03ac49
DoubleBridges <davelkane@gmail.com>
2020-12-03 17:08:43 -0600

    day 3 part 1 reattempt

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,15 +1,16 @@
-with open('./day_3_input.txt', 'r') as slope:
+with open('day_3_input.txt', 'r') as slope:
 	slope_map = []
 	for line in slope:
 		slope_map.append(line.strip('\n'))
-
-	idx = 0
+	
 	width = len(slope_map[0])
+	end_of_slope = len(slope_map)
+	idx = 0
 	height = 0
-	end_of_slope = len(slope_map)-1
 	trees = 0
 
-	while height <= end_of_slope:
+	while height < end_of_slope:
+		print(height, end_of_slope, idx, width, idx % width, slope_map[height][idx % width])
 		trees += 1 if slope_map[height][idx % width] == '#' else 0
 		height += 1
 		idx += 3 
