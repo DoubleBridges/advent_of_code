0feb8b648c7d9b7052494cf3fb2faa14c3dc699b
Dave Kane <davelkane@gmail.com>
2020-12-06 02:14:13 -0600

    day 6 part 2 started

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,4 +1,6 @@
-with open("day_6_input.txt", "r") as answers:
+from collections import Counter
+
+with open("example.txt", "r") as answers:
 
     def grouped_answers(answers):
         answers_by_group = []
@@ -10,7 +12,6 @@ with open("day_6_input.txt", "r") as answers:
                     group_answers.add(answer) if answer != "\n" else 0
 
             else:
-                print(group_answers)
                 answers_by_group.append(group_answers)
                 group_answers = set()
 
@@ -20,6 +21,25 @@ with open("day_6_input.txt", "r") as answers:
     def count_answers_by_group(answers):
         return [len(answer) for answer in answers]
 
-    group_answers = grouped_answers(answers)
+    # group_answers = grouped_answers(answers)
 
     # print(sum(count_answers_by_group(group_answers)))
+
+    def grouped_answers_2(answers):
+        answers_by_group = []
+        group_answers = []
+        group_members = 0
+
+        for answer_set in answers:
+            if answer_set != "\n":
+                for answer in answer_set:
+                    group_answers.append(answer.strip("\n"))
+                    group_members += 1
+                else:
+                    answers_by_group.append(group_answers)
+                    group_answers = []
+
+        answers_by_group.append(group_answers)
+        return answers_by_group
+
+    print(grouped_answers_2(answers))
